d62381e2b25516729f31200ff23b3ede4490fac0
Misganu <misdess@gmail.com>
2022-11-05 13:08:27 +0100

    Navigate to credentials on log out event

--- src/core/features/login/pages/credentials/credentials.ts
+++ src/core/features/login/pages/credentials/credentials.ts
@@ -69,7 +69,7 @@ export class CoreLoginCredentialsPage implements OnInit, OnDestroy {
      * Initialize the component.
      */
     ngOnInit(): void {
-        const siteUrl = CoreNavigator.getRouteParam<string>('siteUrl');
+        const siteUrl = CoreConstants.CONFIG.siteurl;//CoreLoginHelper.getFixedSites();
         if (!siteUrl) {
             CoreDomUtils.showErrorModal('Site URL not supplied.');
             CoreNavigator.back();
